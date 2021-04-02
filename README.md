6396e9d61e6aaf63e727bfdf719d6ee214e05968
YuseiAbe <yabe@diverta.co.jp>
2021-04-02 16:55:35 +0900

    add jest

--- README.md
+++ README.md
@@ -22,6 +22,13 @@ json files are supposed to be exported with [katalon web recorder](https://chrom
 choose `JSON (via plugin)` in each testcase for export json files.  
 ![instruction.gif](./.github/doc/instruction.gif)
 
+## example
+
+we are using Jest snapshot for testing.  
+you can also find an execution result through the snapfile.  
+the result js file would be similar to a [snap](__test__/__snapshots__/main.test.ts.snap) file 
+when the input command is as the same as [test](__test__/main.test.ts).
+
 ## options (for runninng on CI)
 
 `node ${generatedScript} ./new_capture_dir --headless`
---
+++ __test__/main.test.ts
@@ -0,0 +1,35 @@
+import fs from 'fs';
+import * as Transpiler from '../src';
+
+describe('snapshot main transpilation', () => {
+  test('will match snapshot', () => {
+    // arrange
+    fs.mkdirSync = jest.fn().mockReturnValue(() => {});
+    fs.writeFileSync = jest.fn().mockReturnValue(() => {});
+
+    // act
+    Transpiler.create({
+      input: './__test__/etc/test.json',
+      basicAuth: 'userId:password',
+      delay: 500,
+      viewportWidth: 1920,
+      viewportHeight: 1080,
+    });
+
+    // assert
+    const outDir = (fs.mkdirSync as jest.Mock).mock.calls[0][0];
+    const outFile = (fs.writeFileSync as jest.Mock).mock.calls[0][0];
+    const code = (fs.writeFileSync as jest.Mock).mock.calls[0][1];
+
+    // console.dir(outDir);
+    // console.dir(outFile);
+    // console.dir(code);
+
+    expect(fs.mkdirSync).toBeCalled();
+    expect(fs.writeFileSync).toBeCalled();
+
+    expect(outDir).toBe(`${process.cwd()}/__test__/etc`);
+    expect(outFile).toBe(`./__test__/etc/test.e2e.test.js`);
+    expect(code).toMatchSnapshot();
+  });
+});
