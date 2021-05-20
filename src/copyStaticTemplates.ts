96ada93aa6d504848965954e4c908bf9c15fdc22
YuseiAbe <yabe@diverta.co.jp>
2021-05-20 13:38:40 +0900

    chore: remove unused logger

--- src/copyStaticTemplates.ts
+++ src/copyStaticTemplates.ts
@@ -6,7 +6,6 @@ export function copyStaticTemplates(newDirPath: string) {
   glob
     .sync(path.resolve(__dirname, '..', 'template', '*.js'))
     .forEach((staticTemplatePath) => {
-      console.log(newDirPath, staticTemplatePath)
       const dist = path.resolve(newDirPath, path.basename(staticTemplatePath));
       fs.copyFileSync(staticTemplatePath, dist);
     });
