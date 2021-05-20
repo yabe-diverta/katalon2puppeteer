7f972e81e56f89af2e445e45c52851f6cbb59cb5
YuseiAbe <yabe@diverta.co.jp>
2021-05-20 15:04:21 +0900

    docs: add description for task option

--- template/Executor.js
+++ template/Executor.js
@@ -38,15 +38,37 @@ class Executor {
   ) {
     return async () => {
       const behaviorFlag = (await promiseFactory({ ...this })) || {};
+      
+      /**
+       * An options object for switching behavior like either takes screenshots, disable delay, etc.  
+       * This should be returned by each tasks.  
+       * In a normal process generating code from katalon json def, we don't make builtin codes to return this option,  
+       * you can return it as you needed by customizing the target task JS file (so this is optional).
+       * 
+       * @example `
+       * // task/open.0.js
+       * module.exports = async ({ browser, page }) => {
+       *     await page.goto('http://localhost:8080/', { waitUntil: 'networkidle0' })
+       *     await page.waitTillHTMLRendered()
+       * 
+       *     return { disableScreenshot: true }  // <- you would add this line to make the executor disable taking screenshots.
+       * }
+       * `
+       */
+      const {
+        disableDelay,
+        disableScreenshot,
+        delayMilliSecond
+      } = behaviorFlag;
 
-      if (!behaviorFlag.disableDelay) {
+      if (!disableDelay) {
         const delay = (time = 0) => {
           return new Promise((resolve) => setTimeout(resolve, time));
         };
-        await delay(wailtMilliSecond);
+        await delay(delayMilliSecond || wailtMilliSecond);
       }
 
-      if (!behaviorFlag.disableScreenshot) {
+      if (!disableScreenshot) {
         const currentDir = path.dirname(__filename).split(path.sep).pop();
         await this.page.screenshot({
           path: `${this.captureDir}${path.sep}capture.${currentDir}.${idx}.${promiseFactoryName}.png`,
